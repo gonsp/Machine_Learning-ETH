25764d09892e96279e6abdb6b8e75c7e1ca7d041
Gonzalo Solera <gonzalo1796@gmail.com>
2017-11-17 18:33:56 +0100

    Reordering output probability labels

--- ml_project/models/model_wrapper.py
+++ ml_project/models/model_wrapper.py
@@ -4,15 +4,17 @@ from ml_project.pipeline import Pipeline
 
 class ModelWrapper(Pipeline):
     def __init__(self, model, save_path=None):
-        self.model = model
-        super(ModelWrapper, self).__init__(self.model)
+        super(ModelWrapper, self).__init__(model)
 
     def fit(self, X, y=None):
         y_new = np.array([np.argmax(p) for p in y])
         super(ModelWrapper, self).fit(X, y_new)
         return self
 
-    def transform(self, X, y=None):
-        print("This shouldn't happen")
-        exit(1)
-        return X
+    def predict_proba(self, X):
+        label_order = self._final_estimator.classes_
+        y_pred = super(ModelWrapper, self).predict_proba(X)
+        y_sorted = np.array([list(zip(*sorted(zip(label_order, p))))[1] for p in y_pred])
+        return y_sorted
+
+
