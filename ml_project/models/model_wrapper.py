f7fc36a38a712de459a8cb91ba672693d7f43365
Gonzalo Solera <gonzalo1796@gmail.com>
2017-11-18 14:40:43 +0100

    Creating a different label for each order

--- ml_project/models/model_wrapper.py
+++ ml_project/models/model_wrapper.py
@@ -7,14 +7,17 @@ class ModelWrapper(Pipeline):
         super(ModelWrapper, self).__init__(model)
 
     def fit(self, X, y=None):
-        y_new = np.array([np.argmax(p) for p in y])
+        y_new = np.array(["".join([str(i) for i in np.argsort(-p)]) for p in y])
         super(ModelWrapper, self).fit(X, y_new)
         return self
 
     def predict_proba(self, X):
-        label_order = self._final_estimator.classes_
+        labels = self._final_estimator.classes_
         y_pred = super(ModelWrapper, self).predict_proba(X)
-        y_sorted = np.array([list(zip(*sorted(zip(label_order, p))))[1] for p in y_pred])
+        y_sorted = np.array([self.label_to_probability(max(list(zip(labels, p)), key=lambda k: k[1])[0]) for p in y_pred])
         return y_sorted
 
-
+    def label_to_probability(self, label):
+        l = list(zip([0.4, 0.3, 0.2, 0.1], label))
+        l = sorted(l, key=lambda k: k[1])
+        return list(zip(*l))[0]
