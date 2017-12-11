8b17123ca791875bc65bd3f04db498c263103ae8
Gonzalo Solera <gonzalo1796@gmail.com>
2017-12-11 19:04:25 +0100

    Adding CardiogramVisualizer

--- ml_project/models/utils.py
+++ ml_project/models/utils.py
@@ -53,3 +53,53 @@ class MRIVisualizer(BaseEstimator, TransformerMixin):
 
     def transform(self, X, y):
         pass
+
+class CardiogramVisualizer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        n_elements = 6822
+        n_features = 18286
+        X = X.reshape(n_elements, n_features)
+
+        plt.ion()
+        plt.show()
+
+        fig_size = plt.rcParams["figure.figsize"]
+         
+        print("Current size:", fig_size)
+         
+        # Set figure width to 12 and height to 9
+        fig_size[0] = 14
+        fig_size[1] = 5
+        plt.rcParams["figure.figsize"] = fig_size
+
+        first = True
+        id = 0
+        while True:
+            action = True
+
+            if not first:
+                c = sys.stdin.read(1)
+                if c == 'd':
+                    id += 1
+                elif c == 'a':
+                    id -= 1
+                else:
+                    action = False
+
+            first = False
+
+            if action:
+                plt.clf()
+                print("id:", id)
+                print("class: ", y[id])
+                plt.plot(np.trim_zeros(X[id]))
+                plt.show()
+                plt.pause(0.001)
+
+
+
+    def transform(self, X, y):
+        pass
